294a8716ef5eacdb2237f6033976e4e50cdbdc27
GUILHERME DIAS VILLANOVA <04301000003@senacrs.edu.br>
2025-10-27 17:23:33 -0300

    integração com a backend

--- App/pages/CreateTask.tsx
+++ App/pages/CreateTask.tsx
@@ -1,20 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput,TouchableOpacity } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { use, useEffect, useState } from "react";
 
 export default function CreateTask() {
-     const navigation = useNavigation<any>();
+  const navigation = useNavigation<any>();
+
+  const [name, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  const criarTarefa = async () => {
+    try {
+      const resposta = await fetch("http://172.20.86.129:3000/tarefas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, descricao}),
+      });
+
+      if(!resposta.ok){
+        console.log("Erro ao criar tarefa: ", resposta);
+        return
+      }
+
+      navigation.navigate("Task")
+      
+    } catch (e){
+      console.log("houve um erro: ", e)
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text>Criar Tarefa</Text>
-      <TextInput style={styles.input} placeholder="Nome da tarefa"></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome da tarefa"
+        value={name}
+        onChangeText={setNome}
+      ></TextInput>
       <TextInput
         style={styles.input}
         placeholder="Descrição da Tarefa"
+        value={descricao}
+        onChangeText={setDescricao}
       ></TextInput>
       <TouchableOpacity
-        onPress={() => navigation.navigate("Task")}
+        onPress={criarTarefa}
         style={{
           backgroundColor: "#00b7ff70",
           padding: 10,
@@ -26,7 +65,7 @@ export default function CreateTask() {
         }}
       >
         <Text style={{ color: "#fff", fontSize: 15 }}>
-          Criar nova Tarefa +{" "}
+          Criar nova Tarefa +
         </Text>
       </TouchableOpacity>
     </View>
--- App/pages/TaskPage.tsx
+++ App/pages/TaskPage.tsx
@@ -1,109 +1,135 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, FlatList } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import Task from '../components/Task';
-import { useNavigation } from '@react-navigation/native';
-import { useState, useEffect } from 'react';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Task from "../components/Task";
+import { useNavigation } from "@react-navigation/native";
+import { useState, useEffect } from "react";
 
 export default function TaskPage() {
-    const navigation = useNavigation<any>();
+  const navigation = useNavigation<any>();
 
-    const [tarefas, setTarefas] = useState<any[]>([]);
+  const [tarefas, setTarefas] = useState<any[]>([]);
 
-    useEffect(() => {
-        // Criamos uma função assíncrona dentro do useEffect
-        const buscarUsuarios = async () => {
-            try {
-                console.log("Buscando dados da API...");
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const buscarTarefa = async () => {
+        try {
+          console.log("Buscando dados da API...");
 
-                // Faz a requisição e aguarda a resposta
-                const resposta = await fetch("https://jsonplaceholder.typicode.com/todos");
+          // Faz a requisição e aguarda a resposta
+          const resposta = await fetch("http://172.20.86.129:3000/tarefas/");
 
-                // Converte a resposta para JSON
-                const dados = await resposta.json();
+          // Converte a resposta para JSON
+          const dados = await resposta.json();
 
-                // Atualiza o estado com os dados recebidos
-                setTarefas(dados);
-            } catch (erro) {
-                // Captura e mostra erros, se houver
-                console.error("Erro ao buscar usuários:", erro);
-            }
-        };
+          // Atualiza o estado com os dados recebidos
+          setTarefas(dados);
+        } catch (erro) {
+          // Captura e mostra erros, se houver
+          console.error("Erro ao buscar tarefas:", erro);
+        }
+      };
+      buscarTarefa()
+      console.log("oi")
+    }, 10000);
 
-        // Chama a função assíncrona
-        buscarUsuarios();
-    }, []);
+    return () => clearInterval(timer);
+  }, []);
 
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Lista de Tarefas</Text>
 
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.title}>Lista de Tarefas</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Create")}
+          style={{
+            backgroundColor: "#00b7ff70",
+            padding: 10,
+            borderRadius: 10,
+            width: 200,
+            height: 45,
+            alignItems: "center",
+            top: 150,
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 15 }}>
+            Criar nova Tarefa +{" "}
+          </Text>
+        </TouchableOpacity>
 
-                <TouchableOpacity
-                    onPress={() => navigation.navigate("Create")}
-                    style={{ backgroundColor: '#00b7ff70', padding: 10, borderRadius: 10, width: 200, height: 45, alignItems: "center", top: 150 }}
-                >
-                    <Text style={{ color: '#fff', fontSize: 15 }}>Criar nova Tarefa   + </Text>
-                </TouchableOpacity>
-
-                <View style={styles.tasks}>
-                    <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollTask}>
-                        <View style={styles.headerTasks}>
-                            <Text style={styles.taskTitle}>Tarefas</Text>
-                        </View>
-                        <View style={styles.gapTask}>
-                            <FlatList
-                                data={tarefas}
-                                keyExtractor={(item) => item.id.toString()} // chave única
-                                renderItem={({ item }) => (
-                                    <>
-                                        <Task nome="limpar a casa" status={item.completed ? "completado" : "não completado"} descricao={item.title} />
-                                    </>
-                                )}
-                            />
-                        </View>
-                    </ScrollView>
-                </View>
-            </SafeAreaView>
-        </SafeAreaProvider>
-    );
+        <View style={styles.tasks}>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={styles.scrollTask}
+          >
+            <View style={styles.headerTasks}>
+              <Text style={styles.taskTitle}>Tarefas</Text>
+            </View>
+            <View style={styles.gapTask}>
+              <FlatList
+                data={tarefas}
+                keyExtractor={(item) => item.id.toString()} // chave única
+                renderItem={({ item }) => (
+                  <>
+                    <Task
+                      nome={item.name}
+                      status={item.name}
+                      descricao={item.title}
+                    />
+                  </>
+                )}
+              />
+            </View>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#109de9ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 25,
-        color: "white"
-    },
-    tasks: {
-        width: 420,
-        height: 500,
-        position: 'relative',
-        top: 200,
-        borderRadius: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: "white"
-    },
-    scrollTask: {
-        width: 300,
-    },
-    gapTask: {
-        gap: 20
-    },
-    headerTasks: {
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    taskTitle: {
-        fontSize: 26
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#109de9ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 25,
+    color: "white",
+  },
+  tasks: {
+    width: 420,
+    height: 410,
+    position: "relative",
+    top: 200,
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingBottom: 40,
+  },
+  scrollTask: {
+    width: 300,
+  },
+  gapTask: {
+    gap: 10,
+  },
+  headerTasks: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  taskTitle: {
+    fontSize: 26,
+  },
 });
